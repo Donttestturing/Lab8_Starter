7262a8587dbc2f2e1896d50a89a1f6a1a73b13b1
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2022-11-17 18:27:30 -0800

    label 5 functions to test

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -4,3 +4,19 @@ const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
 
+//isPhoneNumber test
+
+
+//isEmail test
+
+
+//isStrongPassword test
+
+
+//isDate test
+
+
+//isHexColor test
+
+
+
